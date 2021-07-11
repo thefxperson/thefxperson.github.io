93a4e22b8368ef71066a01b9c7603dcf295d3a80
Parker <thefxperson@gmail.com>
2021-07-10 20:54:54 -0700

    fixed mobile navbar issues; animated dropdown

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
 function mobileMenu() {
+  //change navbar to dropdown
   var x = document.getElementById("nav_elems");
+  var y = document.getElementById("name");  //adjust name position so there's no conflict
   if (x.className === "topnav") {
     x.className += " responsive";
+    y.style.top = "65%";
   } else {
     x.className = "topnav";
+    y.style.top = "50%";
   }
 }
